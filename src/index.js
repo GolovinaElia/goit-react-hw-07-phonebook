daccdaacd4ab4a5ff46bfe44fd783bc9ad018da3
GolovinaElia <eliagolovina@ukr.net>
2021-06-06 23:14:42 +0300

    Добавляет файлы

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
 import store from './redux/store';
 import App from './App';
 import 'modern-normalize/modern-normalize.css';
@@ -9,12 +8,7 @@ import 'modern-normalize/modern-normalize.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate
-        loading={<h1>Loading...</h1>}
-        persistor={storePersistor.persistor}
-      > */}
       <App />
-      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
